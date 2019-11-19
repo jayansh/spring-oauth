d3878f0d7de4b0d25b0bfa9e1dcf9700ee8297d9
jayansh <jayansh@jaysan.in>
2019-11-19 14:58:38 +0530

    Initial version for spring boot oauth 2 application for google

---
+++ src/main/java/com/mindpath/tech/springoauthgoogle/SpringOauthGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.mindpath.tech.springoauthgoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthGoogleApplication.class, args);
+	}
+
+}
